18b56d9027a304e40f09086175d62f70bc02d2bb
Christopher Laughlin <Chrisgator11@live.com>
2019-06-18 19:17:53 -0400

    scripttest
     d

---
+++ Assets/Scripts/testScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    // if youre reading this ur gay haha
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
